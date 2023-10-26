29a2f37e4fd3908c0e023cb1318ab4a692e3b91e
claire greenwood <claire.greenwood@arenaflowers.com>
2023-10-26 10:35:11 +0100

    fetch articles containing climate from numerous sources using axios

--- index.js
+++ index.js
@@ -2,7 +2,63 @@ const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
-
 const app = express()
+
+const newspapers = [
+    {
+        name: 'The Times',
+        address: 'https://www.thetimes.co.uk/environment/climate-change',
+        base: ''
+    },
+    {
+        name: 'The Guardian',
+        address: 'https://www.theguardian.com/environment/climate-crisis',
+        base: 'https://www.theguardian.com'
+    },
+    {
+        name: 'The Telegraph',
+        address: 'https://www.telegraph.co.uk/climate-change/',
+        base: 'https://www.telegraph.co.uk'
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.co.uk/news/science_and_environment',
+        base: 'https://www.bbc.co.uk',
+    },
+    {
+        name: 'nyt',
+        address: 'https://www.nytimes.com/international/section/climate',
+        base: '',
+    },
+]
+
+const articles = [];
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        $('a:contains("climate")', html).each(function () {
+            const title = $(this).text().trim();
+            const url = $(this).attr('href');
+
+            articles.push({
+                title,
+                url: newspaper.base + url,
+                source: newspaper.name
+            });
+        })
+    })
+})
+
+app.get('/', (req, res) => {
+  res.json('Welcome to my page')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
